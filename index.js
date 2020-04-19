0063e821ff390953c9ed4bf0655ee240cca6e554
Daniel Wachter <scsi@gmx.ch>
2020-04-19 23:21:28 +0200

    Device discovering

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 'use strict';
-const neeoapi = require('neeo-sdk');
-const controller = require('./lib/braviaController');
+const NeeoApi = require('neeo-sdk');
+const BraviaController = require('./lib/braviaController');
 const adapterName = 'Bravia TV IP-Controller';
 const driverVersion = 1;
 console.log('Initialize Neeo Adapter: ' + adapterName + '(Version: ' + driverVersion + ')');
 console.log('-------------------------------------------------');
 
-let braviaDevice = neeoapi.buildDevice(adapterName);
+let braviaDevice = NeeoApi.buildDevice(adapterName);
 braviaDevice.setDriverVersion(driverVersion)
 braviaDevice.setManufacturer('Sony')
              .addAdditionalSearchToken('Bravia REST-API')
              .setType('TV');
 console.log('- initialize buttons');
-for (let [key, value] of controller.tvButtonMappings) {
+for (let [key, value] of BraviaController.tvButtonMappings) {
     //console.debug('add button with key: \'' + key + '\' and label: \'' + value.label + '\'');
     braviaDevice.addButton({name: key, label: value.label });
 }
-braviaDevice.addButtonHander(controller.braviaButtonPressed);
-
+braviaDevice.addButtonHander(BraviaController.braviaButtonPressed);
+braviaDevice.registerInitialiseFunction(BraviaController.initDeviceDiscovering);
 braviaDevice.registerDeviceSubscriptionHandler (
     {
-        deviceAdded: (deviceId) => console.debug('device added', deviceId),
-        deviceRemoved: (deviceId) => console.debug('device removed', deviceId),
+        deviceAdded: (deviceId) => BraviaController.registerDevice(deviceId),
+        deviceRemoved: (deviceId) => BraviaController.deRegisterDevice(deviceId),
         initializeDeviceList: (deviceIds) => console.debug('existing devices', deviceIds),
+    }
+);
+braviaDevice.enableRegistration(
+    {
+        type: 'SECURITY_CODE',
+        headerText: 'DEVICE REGISTRATION',
+        description: 'Please enter the pairing code of your device',
+    },
+    {
+        register: (securityCode) => {
+            console.log ('Code', securityCode);
+            BraviaController.addDeviceCode(securityCode)},
+        isRegistered: () => {console.log('isRegistered')},
     }
 );
 braviaDevice.enableDiscovery(
     {
-        headerText: 'HELLO HEADER',
-        description: 'ADD SOME ADDITIONAL INFORMATION HOW TO PREPARE YOUR DEVICE',
+        headerText: 'Sony Bravia IP-Controller integration',
+        description: 'Compatible Sony Bravia TVs are auto discovered in your local network. Press Next when ready, to select your device. ',
         enableDynamicDeviceBuilder: false,
     },
     function(optionalDeviceId) {
-        Promise.all([controller.discoveredDevices()]).then(function(values) {
-            return values;
-        });
+        console.log('optionalDeviceId: ' + optionalDeviceId);
+        return BraviaController.discoveredDevices();
     }
 );
 module.exports = braviaDevice;
