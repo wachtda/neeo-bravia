7619cbf7677d7f3ee5693bcb8281345106d29d5c
Daniel Wachter <daniel.wachter@jaxfront.com>
2020-04-20 18:03:00 +0200

    Added caching for devices

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 'use strict';
-const PersistencyCacheManager = require('./lib/persistencyCacheManager');
+const NeeoApi = require('neeo-sdk');
+const BraviaController = require('./lib/braviaController');
+const adapterName = 'Bravia TV IP-Controller';
+const driverVersion = 1;
+console.log('Initialize Neeo Adapter: ' + adapterName + '(Version: ' + driverVersion + ')');
+console.log('-------------------------------------------------');
 
-PersistencyCacheManager.deleteStorage().then(() => PersistencyCacheManager.init());
+let braviaDevice = NeeoApi.buildDevice(adapterName);
+braviaDevice.setDriverVersion(driverVersion)
+braviaDevice.setManufacturer('Sony')
+    .addAdditionalSearchToken('Bravia REST-API')
+    .setType('TV');
+console.log('- initialize buttons');
+for (let [key, value] of BraviaController.tvButtonMappings) {
+    //console.debug('add button with key: \'' + key + '\' and label: \'' + value.label + '\'');
+    braviaDevice.addButton({name: key, label: value.label });
+}
+braviaDevice.addButtonHander(BraviaController.braviaButtonPressed);
+braviaDevice.registerInitialiseFunction(BraviaController.init);
+braviaDevice.registerDeviceSubscriptionHandler (
+    {
+        deviceAdded: (deviceId) => BraviaController.registerDevice(deviceId),
+        deviceRemoved: (deviceId) => BraviaController.deRegisterDevice(deviceId),
+        initializeDeviceList: (deviceIds) => console.debug('existing devices', deviceIds),
+    }
+);
+braviaDevice.enableRegistration(
+    {
+        type: 'SECURITY_CODE',
+        headerText: 'DEVICE REGISTRATION',
+        description: 'Please enter the pairing code of your device',
+    },
+    {
+        register: (securityCode) => {
+            console.log ('Code', securityCode);
+            BraviaController.addDeviceCode(securityCode)},
+        isRegistered: () => {console.log('isRegistered')},
+    }
+);
+braviaDevice.enableDiscovery(
+    {
+        headerText: 'Sony Bravia IP-Controller integration',
+        description: 'Compatible Sony Bravia TVs are auto discovered in your local network. Press Next when ready, to select your device. ',
+        enableDynamicDeviceBuilder: false,
+    },
+    function(optionalDeviceId) {
+        console.log('optionalDeviceId: ' + optionalDeviceId);
+        return BraviaController.discoveredDevices();
+    }
+);
+module.exports = braviaDevice;
--- test/test-persistencyCacheManager.js
+++ test/test-persistencyCacheManager.js
@@ -5,7 +5,7 @@ const PersistencyCacheManager = require('../lib/persistencyCacheManager');
 const device1 = {id: 123, name: 'device1'};
 const device2 = {id: 124, name: 'device2'};
 
-it('PersistencyCacheManager init', function(done) {
+it('PersistencyCacheManager test init', function(done) {
     PersistencyCacheManager.deleteStorage().then(() => PersistencyCacheManager.init().then(
         function () {
             done();
@@ -16,7 +16,7 @@ it('PersistencyCacheManager init', function(done) {
     ));
 });
 
-it('PersistencyCacheManager addingDiscoveredDevice', function(done) {
+it('PersistencyCacheManager test DeviceStorageAndCache (DiscoveredDevices)', function(done) {
     PersistencyCacheManager.addDiscoveredDevice(device1.id.toString(), device1);
     PersistencyCacheManager.addDiscoveredDevice(device2.id.toString(), device2);
     const stats = PersistencyCacheManager.DiscoveredCache.stats();
@@ -33,7 +33,7 @@ it('PersistencyCacheManager addingDiscoveredDevice', function(done) {
     }).catch((error) => done(error));
 });
 
-it('PersistencyCacheManager addingRegisteredDevice', function(done) {
+it('PersistencyCacheManager test DeviceStorageAndCache (RegisteredDevices)', function(done) {
     PersistencyCacheManager.addRegisteredDevice(device1.id.toString(), device1);
     PersistencyCacheManager.addRegisteredDevice(device2.id.toString(), device2);
     const stats = PersistencyCacheManager.RegisteredCache.stats();
@@ -50,7 +50,59 @@ it('PersistencyCacheManager addingRegisteredDevice', function(done) {
     }).catch((error) => done(error));
 });
 
-it('PersistencyCacheManager removeRegisteredDevice', function (done) {
+it('PersistencyCacheManager test AddingDevice (DiscoveredDevices)', function(done) {
+    PersistencyCacheManager.addDiscoveredDevice(device1.id.toString(), device1);
+    PersistencyCacheManager.addDiscoveredDevice(device2.id.toString(), device2);
+    const stats = PersistencyCacheManager.DiscoveredCache.stats();
+    expect(stats.count).to.equal(2);
+
+    const expected1 = PersistencyCacheManager.getDiscoveredDevice(device1.id.toString());
+    const expected2 = PersistencyCacheManager.getDiscoveredDevice(device2.id.toString());
+    expect(expected1.id).to.equal(device1.id);
+    expect(expected2.id).to.equal(device2.id);
+    done();
+});
+
+it('PersistencyCacheManager test removeDevice (DiscoveredDevices)', function (done) {
+    const expectedCacheBefore = PersistencyCacheManager.DiscoveredCache.stats().count;
+    let expectedCacheAfter;
+    let expectedStorageBefore;
+    let expectedStorageAfter;
+    PersistencyCacheManager.DiscoveredStorage.length()
+        .then((before) => {
+            expectedStorageBefore = before;
+        })
+        .then(() => {
+            PersistencyCacheManager.removeDiscoveredDevice(device1.id.toString())
+                .then(() => {
+                    PersistencyCacheManager.DiscoveredStorage.length().then((after) => {
+                        expectedStorageAfter = after;
+                        expectedCacheAfter = PersistencyCacheManager.DiscoveredCache.stats().count;
+                    }).then(() => {
+                        expect(expectedStorageBefore).to.equal(2);
+                        expect(expectedCacheBefore).to.equal(2);
+                        expect(expectedStorageAfter).to.equal(1);
+                        expect(expectedCacheAfter).to.equal(1);
+                        done();
+                    }).catch((error) => done(error));
+                });
+        });
+});
+
+it('PersistencyCacheManager test AddingDevice (RegisteredDevices)', function(done) {
+    PersistencyCacheManager.addRegisteredDevice(device1.id.toString(), device1);
+    PersistencyCacheManager.addRegisteredDevice(device2.id.toString(), device2);
+    const stats = PersistencyCacheManager.RegisteredCache.stats();
+    expect(stats.count).to.equal(2);
+
+    const expected1 = PersistencyCacheManager.getRegisteredDevice(device1.id.toString());
+    const expected2 = PersistencyCacheManager.getRegisteredDevice(device2.id.toString());
+    expect(expected1.id).to.equal(device1.id);
+    expect(expected2.id).to.equal(device2.id);
+    done();
+});
+
+it('PersistencyCacheManager test removeDevice (RegisteredDevices)', function (done) {
     const expectedCacheBefore = PersistencyCacheManager.RegisteredCache.stats().count;
     let expectedCacheAfter;
     let expectedStorageBefore;
